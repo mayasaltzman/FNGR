93da087216091d0f1916a42df18613c8251812ff
Maya Saltzman <mayasaltzman3@gmail.com>
2025-11-05 16:53:41 -0500

    add name text input

--- lib/create_profile_page.dart
+++ lib/create_profile_page.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
+class ProfileForm extends StatefulWidget {
+  const ProfileForm({super.key});
+
+  @override
+  State<ProfileForm> createState() => _ProfileFormState();
+}
+
+class _ProfileFormState extends State<ProfileForm> {
+  //state for managing form
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  //store name entered
+  String _name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return (SizedBox(
+      width: 375,
+      child: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            const Text("Name"),
+            SizedBox(
+                width: 200,
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: "Name"),
+                ))
+          ])
+        ],
+      ),
+    ));
+    //   Scaffold(
+    //     body: Center(
+    //       child: Column(
+    //         children: [
+    //           const Text("Name"),
+    //           TextFormField(
+    //             decoration: const InputDecoration(labelText: "Name"),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   )
+    // );
+  }
+}
+
 class CreateProfilePage extends StatefulWidget {
   const CreateProfilePage({super.key});
 
   @override
   State<CreateProfilePage> createState() => _CreateProfilePageState();
 }
-
 
 class _CreateProfilePageState extends State<CreateProfilePage> {
   @override
@@ -17,23 +63,13 @@ class _CreateProfilePageState extends State<CreateProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      appBar: AppBar(
-        title: const Text("Create Profile"),
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        leading: const BackButton(), //need to make this actually do something and give it color change for press
-      ),
-      // body: const Center(
-      //     child: Column(
-      //     spacing: 20,
-      //     children: [
-      //       Text("Name, Age"),
-      //       AboutMe(),
-      //       KeyInfo(),
-      //       Preferences(),
-      //       Interests()
-      //     ],
-      // )),
-    );
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: const Text("Create Profile"),
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          leading:
+              const BackButton(), //need to make this actually do something and give it color change for press
+        ),
+        body: const ProfileForm());
   }
 }
