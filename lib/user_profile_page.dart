c6ecb360a1da526fbe8f3954fdfe4dd6b2b10c4d
Maya Saltzman <mayasaltzman3@gmail.com>
2025-11-12 13:02:20 -0500

    about me rendering db info

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -12,49 +12,34 @@ abstract class ProfileStyles {
   static const containerWidth = 375.0;
 }
 
-class AboutMe extends StatefulWidget {
-  const AboutMe({super.key});
+class ProfileInfo extends StatefulWidget {
+  final String bio;
+  const ProfileInfo({super.key, required this.bio});
+  // const ProfileInfo({super.key});
 
   @override
-  State<AboutMe> createState() => _AboutMeState();
+  State<ProfileInfo> createState() => _ProfileInfoState();
 }
 
-//about me box
-class _AboutMeState extends State<AboutMe> {
-  //instance of firebase
-  final firestoreInstance = FirebaseFirestore.instance;
-  late DocumentSnapshot snapshot; //Define snapshot
-
-  //get data from firestore
-  void _getData() async {
-    final data = await firestoreInstance
-        .collection("users")
-        .doc(
-            '1a4dZXRtA80tkguo1REw') //needs to be passed as ID based on user that was clicked this is just for testing
-        .get(); //get the data
-    snapshot = data;
-    print(snapshot['age']);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getData(); //loads data as soon as page opens
-  }
-
-  final String aboutText =
-      "testing text until db connection"; //will replace with stuff from db
-
+// //about me box
+class _ProfileInfoState extends State<ProfileInfo> {
   @override
   Widget build(BuildContext context) {
+    //doesn't render until data is loaded
     return Container(
       width: ProfileStyles.containerWidth,
       decoration: ProfileStyles.boxDecoration,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("About Me", style: Theme.of(context).textTheme.bodyMedium),
-          Text(aboutText, style: Theme.of(context).textTheme.bodySmall)
+          SizedBox(
+            child: Column(
+              children: [
+                Text("About Me", style: Theme.of(context).textTheme.bodyMedium),
+                Text(widget.bio, style: Theme.of(context).textTheme.bodySmall)
+              ],
+            ),
+          ) // render nothing if no bio
         ],
       ),
     );
@@ -120,9 +105,30 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfilePage> {
+  //to display loading state until data loads
+  bool _isLoading = true;
+  //instance of firebase
+  final firestoreInstance = FirebaseFirestore.instance;
+  DocumentSnapshot? snapshot; //Define snapshot
+
+  //get data from firestore
+  void _getData() async {
+    final data = await firestoreInstance
+        .collection("users")
+        .doc(
+            '1a4dZXRtA80tkguo1REw') //needs to be passed as ID based on user that was clicked this is just for testing
+        .get(); //get the data
+    snapshot = data;
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    _getData(); //loads data as soon as page opens
   }
 
   @override
@@ -135,17 +141,25 @@ class _UserProfilePageState extends State<UserProfilePage> {
             const BackButton(), //need to make this actually do something and give it color change for press
         actions: [TextButton(onPressed: () {}, child: const Text("Message"))],
       ),
-      body: const Center(
-          child: Column(
-        spacing: 20,
-        children: [
-          Text("Name, Age"),
-          AboutMe(),
-          KeyInfo(),
-          Preferences(),
-          Interests()
-        ],
-      )),
+      body: Center(
+          child: _isLoading
+              ? const CircularProgressIndicator()
+              : Column(
+                  spacing: 20,
+                  children: [
+                    snapshot!['name'] != null && snapshot!['age'] != null
+                        ? SizedBox(
+                            child: Text(
+                                "${snapshot!['name']}, ${snapshot!['age']}"))
+                        : const SizedBox(),
+                    snapshot!['bio'] != null
+                        ? ProfileInfo(bio: snapshot!['bio'])
+                        : const SizedBox()
+                    // KeyInfo(),
+                    // Preferences(),
+                    // Interests()
+                  ],
+                )),
     );
   }
 }
