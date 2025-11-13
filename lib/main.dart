c6ea3168802306bf9f4fe2acbd5abba90448c86d
Claire Shepherd <shepherc@uoguelph.ca>
2025-11-12 22:01:47 -0500

    abt to merge yall

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       // home: const UserProfilePage(),
-      home:  CreateAccount(),
+      home:  LoginPage(),
       );
      
   }
