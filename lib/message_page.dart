7c8dd6bc0f606096af31605e237ec746ae84843d
Maya Saltzman <mayasaltzman3@gmail.com>
2025-11-16 15:24:53 -0500

    update app bar to be specific for messages

--- lib/message_page.dart
+++ lib/message_page.dart
@@ -56,8 +56,30 @@ class _MessagePageState extends State<MessagePage> {
           backgroundColor: Theme.of(context).colorScheme.secondary,
           leading:
               BackButton(color: Theme.of(context).colorScheme.secondaryFixed),
+          centerTitle: true,
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //replace with info from actual user
+              const CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                    "https://randomuser.me/api/portraits/women/2.jpg",
+                  )),
+              Text("Name", style: TextStyle(fontSize: 12, color: Theme.of(context).colorScheme.secondaryFixed))
+              
+            ],
+          ),
           actions: [
-            TextButton(onPressed: () {}, child: const Text("Message Requests"))
+            //video call button maybe a future element :/
+            //IconButton(
+            // onPressed: () {},
+            // icon: const Icon(Icons.video_call_rounded),
+            // color: Theme.of(context).colorScheme.secondaryFixed),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_horiz),
+                color: Theme.of(context).colorScheme.secondaryFixed),
           ],
         ));
     // body: const SingleChildScrollView(
