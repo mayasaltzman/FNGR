681f94e3980b353a9659af577f4b3e05030ef726
Maya Saltzman <mayasaltzman3@gmail.com>
2025-11-16 16:07:43 -0500

    messages have lesbian themeing

--- lib/message_page.dart
+++ lib/message_page.dart
@@ -21,11 +21,33 @@ class MessageWidgetState extends State<MessageWidget> {
 
   @override
   Widget build(BuildContext context) {
+    //replace users with db data
     return Scaffold(
       //this is from https://pub.dev/packages/flutter_chat_core/example
+      //more documentation how to work with chat https://flyer.chat/docs/flutter/guides/dynamic-theming/
       body: Chat(
         chatController: _chatController,
         currentUserId: 'user1',
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        //builders allow for styling of message theme
+        //need to test what another user message looks like once logs are created 
+        builders: Builders(
+          textMessageBuilder: (
+            context,
+            message,
+            index, {
+            required bool isSentByMe,
+            MessageGroupStatus? groupStatus,
+          }) {
+            return SimpleTextMessage(
+              message: message,
+              index: index,
+              sentBackgroundColor: Theme.of(context).colorScheme.tertiaryFixed,
+              sentTextStyle: TextStyle(color: Colors.grey[900]),
+              timeStyle: TextStyle(color: Colors.grey[600], fontSize: 10),
+            );
+          },
+        ),
         onMessageSend: (text) {
           _chatController.insertMessage(
             TextMessage(
@@ -89,7 +111,6 @@ class _MessagePageState extends State<MessagePage> {
                 color: Theme.of(context).colorScheme.secondaryFixed),
           ],
         ),
-        body: const MessageWidget()
-        );
+        body: const MessageWidget());
   }
 }
