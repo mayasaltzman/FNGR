e9af4e61b0b7e9d69a02b1ec2861f3562ec62011
Claire Shepherd <shepherc@uoguelph.ca>
2025-11-22 16:54:51 -0500

    incoperate forget password button

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -8,7 +8,7 @@ abstract class ProfileStyles {
   //styles for boxes
   static BoxDecoration get boxDecoration => BoxDecoration(
     color: const Color.fromARGB(255, 255, 255, 255),
-    borderRadius: BorderRadius.circular(15.0),
+    borderRadius: BorderRadius.circular(15),
     border: Border.all(
       color: const Color.fromARGB(255, 214, 212, 210),
       width: 1, 
@@ -17,7 +17,7 @@ abstract class ProfileStyles {
   static ButtonStyle get buttonStyle => ElevatedButton.styleFrom(
     backgroundColor: const Color(0xFFD461A6),
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15.0),
+      borderRadius: BorderRadius.circular(15),
       side: const BorderSide(
         color: Color.fromARGB(255, 214, 212, 210),
         width: 1,
@@ -103,160 +103,177 @@ class _LoginState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFE0CA),
-      //body: Center(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding( 
-                padding: const EdgeInsets.only(top: 200),             
-                child: Text(
-                  "FNGR", 
-                  style: ProfileStyles.boxHeader,
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding( 
+              padding: const EdgeInsets.only(top: 200),             
+              child: Text(
+                'FNGR', 
+                style: ProfileStyles.boxHeader,
               ),
+            ),
 
-              Padding(
-                padding: const EdgeInsets.only(right: 320.0, top: 40),              
-                child: Text(
-                  "Email",
-                  style: ProfileStyles.instructionText,
-                ),
+            Padding(
+              padding: const EdgeInsets.only(right: 320, top: 40),              
+              child: Text(
+                'Email',
+                style: ProfileStyles.instructionText,
               ),
-                        
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0, top: 5),
-                child: Container(  
-                  decoration: ProfileStyles.boxDecoration,
-                  padding: ProfileStyles.boxPadding,
-                  width: ProfileStyles.containerWidth,            
-                  child: TextField(
-                    controller: _emailController,
-                    style: ProfileStyles.boxText,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter your email',
-                      hintStyle: ProfileStyles.boxText,
-                    ),
-                  ), 
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.only(right: 290.0, top: 10),              
-                child: Text(
-                  "Password",
-                  style: ProfileStyles.instructionText,
-                ),
-              ),
-
-              Padding( 
-                padding: const EdgeInsets.only(left: 10.0, top: 5),             
-                child: Container(
-                  decoration: ProfileStyles.boxDecoration,
-                  padding: ProfileStyles.boxPadding,
-                  width: ProfileStyles.containerWidth, 
-                  child: TextField(
-                    controller: _passwordController,
-                    style: ProfileStyles.boxText,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter your password',
-                      hintStyle: ProfileStyles.boxText,
-                    ),
-                  ), 
-                ),
-              ),
-              
-              Padding(
-                padding: const EdgeInsets.only(top: 20, left: 15),
-                child: Row(
-                  children: [
-                    Checkbox(
-                      value: rememberMe,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          rememberMe = value ?? false;
-                        });
-                      },
-                      activeColor: const Color(0xFFD461A6),
-                      checkColor: Colors.white,
-                    ),
-                    const Text(
-                      "Remember Me",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-
-
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Container( 
-                  padding: ProfileStyles.boxPadding,
-                  width: ProfileStyles.containerWidth,             
-                  child: ElevatedButton(
-                    style: ProfileStyles.buttonStyle,
-                    onPressed: isLoading ? null : () {
-                      _signIn();
-                    },
-                    child: Text(
-                      "Login",
-                      style: ProfileStyles.buttonText,
-                    ),
+            ),
+                      
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 5),
+              child: Container(  
+                decoration: ProfileStyles.boxDecoration,
+                padding: ProfileStyles.boxPadding,
+                width: ProfileStyles.containerWidth,            
+                child: TextField(
+                  controller: _emailController,
+                  style: ProfileStyles.boxText,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter your email',
+                    hintStyle: ProfileStyles.boxText,
                   ),
-                ),
+                ), 
               ),
- 
-              const Padding(
-                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),  
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        "Don't have an account?",
-                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w200),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black,
-                        thickness: 0.5,
-                      ),
-                    ),
-                  ],
-                ),
-              ), 
- 
-              Padding(
-                padding: const EdgeInsets.only(top:   10.0),
-                child: Container(
-                  padding: ProfileStyles.boxPadding,
-                  width: ProfileStyles.containerWidth,
-                  child: ElevatedButton(
-                    style: ProfileStyles.buttonStyle,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(right: 290, top: 10),              
+              child: Text(
+                'Password',
+                style: ProfileStyles.instructionText,
+              ),
+            ),
+
+            Padding( 
+              padding: const EdgeInsets.only(left: 10, top: 5),             
+              child: Container(
+                decoration: ProfileStyles.boxDecoration,
+                padding: ProfileStyles.boxPadding,
+                width: ProfileStyles.containerWidth, 
+                child: TextField(
+                  controller: _passwordController,
+                  style: ProfileStyles.boxText,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter your password',
+                    hintStyle: ProfileStyles.boxText,
+                  ),
+                ), 
+              ),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 15),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: rememberMe,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        rememberMe = value ?? false;
+                      });
+                    },
+                    activeColor: const Color(0xFFD461A6),
+                    checkColor: Colors.white,
+                  ),
+                  const Text(
+                    'Remember Me',
+                    style: TextStyle(fontSize: 16, color: Color(0xFF1F1F1F), fontWeight: FontWeight.w200), 
+                  ),
+                  
+                  TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const CreateAccount()));
+                      print('Clicked! - go to change password backend');
                     },
-                    child: Text(
-                      "Create account", 
-                      style: ProfileStyles.buttonText,
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.only(left: 80),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     ),
+                    child: const Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline, 
+                        decorationColor: Color(0xFF9F497D),
+                        color:  Color(0xFF9F497D),                  
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Container( 
+                padding: ProfileStyles.boxPadding,
+                width: ProfileStyles.containerWidth,             
+                child: ElevatedButton(
+                  style: ProfileStyles.buttonStyle,
+                  onPressed: isLoading ? null : () {
+                    _signIn();
+                  },
+                  child: Text(
+                    'Login',
+                    style: ProfileStyles.buttonText,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 10),  
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      "Don't have an account?",
+                      style: TextStyle(color: Color(0xFF1F1F1F), fontWeight: FontWeight.w200),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 0.5,
+                    ),
+                  ),
+                ],
+              ),
+            ), 
+
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                padding: ProfileStyles.boxPadding,
+                width: ProfileStyles.containerWidth,
+                child: ElevatedButton(
+                  style: ProfileStyles.buttonStyle,
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const CreateAccount()));
+                  },
+                  child: Text(
+                    'Create account', 
+                    style: ProfileStyles.buttonText,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      );
-    //);
+      ),
+    );
   }
 }
