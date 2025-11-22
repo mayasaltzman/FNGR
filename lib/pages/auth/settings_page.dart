9896869f674c161625ba62a4569ac8cc45333af6
Claire Shepherd <shepherc@uoguelph.ca>
2025-11-21 22:08:06 -0500

    import firebase signout function

--- lib/pages/auth/settings_page.dart
+++ lib/pages/auth/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import '../../pages/auth/create_account.dart';
+import 'package:test_milestone/pages/auth/login_page.dart';
 import '../../services/firebase_service.dart';
-import '../../main.dart'; // Import NavMenu
+import '../../main.dart'; // Import Login Page
 
 //styles for the page
 abstract class ProfileStyles {
@@ -35,31 +35,24 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsState extends State<SettingsPage> {
-  /*final FirebaseService _firebaseService = FirebaseService();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController  = TextEditingController();
+  final FirebaseService _firebaseService = FirebaseService();
+  //final TextEditingController _emailController = TextEditingController();
+  //final TextEditingController _passwordController  = TextEditingController();
   bool isLoading = false;
 
-  Future<void> _signIn() async {
-     if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
-      _showSnackBar('Please fill in all fields');
-      return;
-    }
+  Future<void> _signOut() async {
     setState(() {
       isLoading = true;
     });
     try {
-      await _firebaseService.signIn(
-        email: _emailController.text.trim(),
-        password:_passwordController.text.trim(),
-      );
+      await _firebaseService.signOut();
       if (mounted) {
         Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const NavMenu()),
+          MaterialPageRoute(builder: (context) => const LoginPage()),
         );
       }
     } catch (e) {
-      _showSnackBar('Login failed: $e');
+      _showSnackBar('Logout failed: $e');
     } finally {
       setState(() {
         isLoading = false;
@@ -75,10 +68,10 @@ class _SettingsState extends State<SettingsPage> {
 
   @override
   void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
+    //_emailController.dispose();
+    //_passwordController.dispose();
     super.dispose();
-  }*/
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,24 +85,29 @@ class _SettingsState extends State<SettingsPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const SizedBox(height: 35),
                 Text(
                   "Settings",
                   style: ProfileStyles.boxHeader.copyWith(
                     fontSize: 26 / MediaQuery.of(context).textScaleFactor,
                   ),
                 ),
-                const SizedBox(height: 30),
-
-                /*const SizedBox(height: 30),
+                Divider(
+                  thickness: 3,
+                  color: Theme.of(context).colorScheme.secondary,
+                  height: 40,
+                ),
+                
+                const SizedBox(height: 640),
                 ElevatedButton(
                   onPressed: isLoading ? null : () {
-                    //_signIn();
-                    },
+                    _signOut();
+                  },
                   child: Text(
-                    "Login",
+                    "Logout",
                     style: ProfileStyles.boxText,
                   ),
-                ),*/
+                ),
               ],
             ),
           )
