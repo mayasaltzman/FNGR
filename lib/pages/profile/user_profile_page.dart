e2ebf9c3dba3e45925889ab1ab57bed951897bdd
nstarkma <nstarkma@uoguelph.ca>
2025-11-26 22:34:34 -0500

    Centered appbar elements and added conditionals for title

--- lib/pages/profile/user_profile_page.dart
+++ lib/pages/profile/user_profile_page.dart
@@ -436,13 +436,14 @@ class _UserProfilePageState extends State<UserProfilePage> {
       backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.secondary,
-        leading: BackButton(
-            color: Theme.of(context)
-                .colorScheme
-                .secondaryFixed), //need to make this actually do something and give it color change for press
+        leading: isUserProfile
+            ? null
+            : BackButton(
+                color: Theme.of(context).colorScheme.secondaryFixed,
+              ),
         centerTitle: true,
         title: Text(
-          'FNGR',
+          isUserProfile ? 'Profile' : 'FNGR',
           style:
               TextStyle(color: Theme.of(context).colorScheme.secondaryFixed),
         ),
